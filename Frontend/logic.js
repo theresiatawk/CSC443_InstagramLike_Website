fc2b1c614e1861f136c8f9224740ad5b6fc8de14
Theresia Tawk <theresiatawk@hotmail.com>
2022-11-19 09:52:37 +0200

    Implementing Comment Logic

--- Frontend/logic.js
+++ Frontend/logic.js
@@ -1,9 +1,12 @@
 const card1 = document.getElementById("card1");
 const card2 = document.getElementById("card2");
 const card3 = document.getElementById("card3");
-const likes = document.querySelectorAll(".heart-img");
+const like_icons = document.querySelectorAll(".heart-img");
+const comment_card = document.getElementById("add_comment");
+const remove_icon = document.getElementById("remove_icon");
+const comment_icons = document.querySelectorAll(".comment-img");
+const add_comment_btn = document.getElementById("add_comment");
 
- 
 
 const goToLoginHandler = () =>{
     card1.style.display = "none";
@@ -21,11 +24,22 @@ const likeHandler = (e) =>{
     if( e.srcElement.attributes[1].value == "./Assets/full_heart.png"){
         e.srcElement.attributes[1].value = "./Assets/empty_heart.png"
     }
-    //like
+    //Otherwise like
     else{
         e.srcElement.attributes[1].value = "./Assets/full_heart.png"
     }
 }
+const commentHandler = () => {
+    comment_card.style.display = "flex";
+}
+const removeCommentCardHandler = (e) => {
+    comment_card.style.display = "none";
+}
 
-likes.forEach(b => b.addEventListener("click", likeHandler));
+
+like_icons.forEach(b => b.addEventListener("click", likeHandler));
+comment_icons.forEach(b => b.addEventListener("click", commentHandler));
+remove_icon.addEventListener("click", removeCommentCardHandler);
+add_comment_btn.addEventListener("click", removeCommentCardHandler);
+
 
