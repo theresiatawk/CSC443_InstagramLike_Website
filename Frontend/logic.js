b1f36a6ea774b5bfd36d4c392da645051d70f13d
Theresia Tawk <theresiatawk@hotmail.com>
2022-11-19 08:22:49 +0200

    Implementing Like Logic

--- Frontend/logic.js
+++ Frontend/logic.js
@@ -1,6 +1,8 @@
 const card1 = document.getElementById("card1");
 const card2 = document.getElementById("card2");
 const card3 = document.getElementById("card3");
+const likes = document.querySelectorAll(".empty-heart-img");
+ 
 
 const goToLoginHandler = () =>{
     card1.style.display = "none";
@@ -13,6 +15,18 @@ const goToSignupHandler =() =>{
     card2.style.display = "none";
     card3.style.display = "flex";
 }
-const likeImageHandler = () => {
-
+const likeHandler = (e) =>{
+    if( e.srcElement.attributes[1].value == "./Assets/full_heart.png"){
+        console.log("YESS");
+        e.srcElement.attributes[1].value = "./Assets/empty_heart.png"
+    }
+    else{
+        console.log("NO");
+        e.srcElement.attributes[1].value = "./Assets/full_heart.png"
+    }
 }
+
+console.log(likes);
+console.log(unlikes);
+likes.forEach(b => b.addEventListener("click", likeHandler));
+
